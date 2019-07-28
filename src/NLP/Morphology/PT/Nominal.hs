7e7ce49c54ec460f478f9e60558e148eea507ab4
Juliano Paiva Junho <jjunho@gmail.com>
2019-07-28 11:31:17 +0900

    Nominal structure

--- src/NLP/Morphology/PT/Nominal.hs
+++ src/NLP/Morphology/PT/Nominal.hs
@@ -6,6 +6,13 @@ module NLP.Morphology.PT.Nominal
   )
   where
 
+import           Data.Text                   (Text)
+import qualified Data.Text                   as T
+import qualified Data.Text.IO                as TIO
+import           NLP.Morphology.PT.Common
+import           NLP.Morphology.PT.Nominal.Base
+import           NLP.Morphology.Txt
+
 noun = undefined
 
 adjective = undefined
--- src/NLP/Morphology/PT/Nominal/Base.hs
+++ src/NLP/Morphology/PT/Nominal/Base.hs
@@ -1,1 +1,21 @@
 module NLP.Morphology.PT.Nominal.Base where
+
+import NLP.Morphology.PT.Common
+
+data NStructure
+  = Nom { root          :: Root
+        , thematicVowel :: ThematicVowel
+        , gender        :: Gender
+        , number        :: Number
+        }
+  | Pro { root          :: Root
+        , thematicVowel :: ThematicVowel
+        , person        :: Person
+        , gender        :: Gender
+        , number        :: Number
+        }
+  | Det { root          :: Root
+        , thematicVowel :: ThematicVowel
+        , gender        :: Gender
+        , number        :: Number
+        }
